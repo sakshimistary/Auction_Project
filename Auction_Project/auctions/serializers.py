c94be49b1508274cdc79c467f8e1570ff030ede1
sakshiM <sakshi.mistary@gmail.com>
2026-04-15 11:27:15 +0530

    Added CreateAuctionView

--- Auction_Project/auctions/serializers.py
+++ Auction_Project/auctions/serializers.py
@@ -27,21 +27,18 @@ class BidSerializer(serializers.ModelSerializer):
     def validate(self, data):
         auction = data.get('auction')
         amount = data.get('amount')
-
         
         if not auction.is_active:
             raise serializers.ValidationError("Auction is not active")
-
         
         if auction.end_time <= timezone.now():
             raise serializers.ValidationError("Auction has ended")
 
-        
         if amount <= auction.current_price:
             raise serializers.ValidationError(f"Bid must be greater than current price ({auction.current_price})")
-
         
         if auction.owner == self.context['request'].user:
             raise serializers.ValidationError("You cannot bid on your own auction")
         
         return data
+    
--- Auction_Project/auctions/views.py
+++ Auction_Project/auctions/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from django.http import JsonResponse
+from auctions.serializers import AuctionSerializer, BidSerializer 
+from rest_framework.permissions import IsAuthenticated 
 # Create your views here.
+
+class CreateAuctionView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        serializer = AuctionSerializer(data=request.data, context={'request': request})
+
+        if serializer.is_valid():
+            serializer.save(owner=request.user)
+            return JsonResponse({"message": "Auction created successfully","data": serializer.data}, status=201)
+
+        return JsonResponse(serializer.errors, status=400)
+    
+
