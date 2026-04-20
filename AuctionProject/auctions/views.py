4b1d74f702410f21bd71dc78107e07894c02ac19
sakshiM <sakshi.mistary@gmail.com>
2026-04-20 15:15:21 +0530

    Token authentication

--- AuctionProject/auctions/views.py
+++ AuctionProject/auctions/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from auctions.models import Auction, Bid
+from auctions.serializers import AuctionSerializer, BidSerializer
+from rest_framework.response import Response
 
-# Create your views here.
+class AuctionView(viewsets.ViewSet):
+    def list(self, request):
+        auctions = Auction.filter(is_active = "is_active")
+
+        serializer = AuctionSerializer(auctions, many = True)
+
+        return Response(serializer.data)
+
